7fb4cefe3d55f418e0e3eecaf63eb890daa593ad
noaeker <n12345e@gmail.com>
2022-01-17 11:08:39 +0200

    small fix

--- unify_current_jobs_results.py
+++ unify_current_jobs_results.py
@@ -8,7 +8,7 @@ def main():
     parser.add_argument('--n_jobs', action='store', type=int,)
     parser.add_argument('--output_folder', action='store', type=str)
     args = parser.parse_args()
-    csvs_path_list = [f'{args.folder}/{n}{CSV_SUFFIX}' for n in range(args.n_jobs)]
+    csvs_path_list = [f'{args.folder}/job_{n}/{n}{CSV_SUFFIX}' for n in range(args.n_jobs)]
     output_csv_path = os.path.join(args.output_folder, "all_jobs_tmp_results.tsv")
     add_csvs_content(csvs_path_list,output_csv_path)
 
