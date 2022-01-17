766c3dd6d93d433032b0443c91cfb83e51b50ff5
noaeker <n12345e@gmail.com>
2022-01-17 11:04:31 +0200

    fixing unify current jobs

--- unify_current_jobs_results.py
+++ unify_current_jobs_results.py
@@ -6,10 +6,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--folder', action='store', type=str)
     parser.add_argument('--n_jobs', action='store', type=int,)
-    parser.add_argument('--output_folder', action='store', type=int)
+    parser.add_argument('--output_folder', action='store', type=str)
     args = parser.parse_args()
-    print(args)
-    csvs_path_list = [f'{args.folder}/{n}.tsv' for n in range(args.n_jobs)]
+    csvs_path_list = [f'{args.folder}/{n}.{CSV_SUFFIX}' for n in range(args.n_jobs)]
     output_csv_path = os.path.join(args.output_folder, "all_jobs_tmp_results.tsv")
     add_csvs_content(csvs_path_list,output_csv_path)
 
