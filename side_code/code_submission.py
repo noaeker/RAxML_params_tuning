66bc3be600aa033b8432905c3f9ea6337be2cac8
noaeker <n12345e@gmail.com>
2022-05-11 15:31:02 +0300

    small fix

--- side_code/code_submission.py
+++ side_code/code_submission.py
@@ -18,7 +18,8 @@ def is_job_done(job_log_folder):
     return False
 
 def execute_command_and_write_to_log(command, print_to_log=False):
-    logging.debug(f"About to run: {command}")
+    if print_to_log:
+        logging.debug(f"About to run: {command}")
     subprocess.run(command, shell=True, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
 
 def generate_argument_list(args):
