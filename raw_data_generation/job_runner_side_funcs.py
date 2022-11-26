9b72860d4e2b43f297cf1296cab66be99a2fb224
noaeker <n12345e@gmail.com>
2022-11-26 21:59:44 +0200

    fix

--- raw_data_generation/job_runner_side_funcs.py
+++ raw_data_generation/job_runner_side_funcs.py
@@ -26,7 +26,7 @@ def main_parser():
     parser.add_argument('--spr_cutoff_grid', action='store', type=str, default=SPR_CUTOFF_GRID)
     parser.add_argument('--trim_msa',action='store_true')
     parser.add_argument('--remove_output_files',action='store_true', default= True)
-    parser.add_argument('--use_existing_global_data',action='store_true', default= True)
+    parser.add_argument('--use_existing_global_data',action='store_true')
     parser.add_argument('--existing_global_data_to_use', type=str, default = "pandit_tuning")
     parser.add_argument('--max_n_parallel_jobs', action='store', type=int, default=N_JOBS)
     parser.add_argument('--print_commands_to_log', action='store_true')
---
+++ raw_data_generation/parse_TREEBASE_database.py
@@ -0,0 +1,15 @@
+from side_code.config import *
+from side_code.file_handling import create_dir_if_not_exists
+import re
+import os
+
+
+
+def main():
+   main_folder = '/Users/noa/Workspace/data/Dana/groups_4/itay_mayrose/danaazouri/PhyAI/submission_data/training_data'
+   for folder in
+
+
+if __name__ == "__main__":
+    main()
+
