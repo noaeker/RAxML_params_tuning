010eea98182d6af0e110eddbd0d4ea6b1027cadb
noaeker <n12345e@gmail.com>
2023-01-14 13:11:35 +0200

    change grid search params

--- ML_pipeline/ML_config.py
+++ ML_pipeline/ML_config.py
@@ -1,14 +1,15 @@
 GENERAL_PARAM_GRID = {
     'n_estimators': [100,300],
     'max_depth': [-1,3,6,12],
-    'num_leaves': [25, 50, 100,150, 200],
+    'num_leaves': [25, 50, 100, 200],
     'learning_rate':[0.001,0.01,0.1],
     #'reg_alpha': [0, 10, 25, 50, 100],
     #'reg_lambda': [0, 10, 25, 50, 100],
     #'min_split_gain': [0.3, 0.4],
     'subsample': [0.6, 0.8, 1],
     'subsample_freq': [20],
-    'boosting_type': ['gbdt','rf','dart']}
+    #'boosting_type': ['gbdt','rf','dart']
+}
 
 
 CLASSIFICATION_PARAM_GRID = {
