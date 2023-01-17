8d07a68bb778cae4ebcd2181fb7f3d3ca126e510
noaeker <n12345e@gmail.com>
2023-01-17 16:34:49 +0200

    small fix

--- ML_pipeline/ML_pipeline_groups_runner.py
+++ ML_pipeline/ML_pipeline_groups_runner.py
@@ -276,8 +276,8 @@ def main():
     if args.add_sample_fracs:
         for sample_frac in  sample_fracs:
             ML_pipeline(results, args, curr_run_dir, sample_frac, RFE=False, large_grid= False,include_output_tree_features = args.include_output_tree_features)
-        if not LOCAL_RUN:
-            ML_pipeline(results, args, curr_run_dir, sample_frac=1.0, RFE=True, large_grid = True, include_output_tree_features= args.include_output_tree_features)
+    if not LOCAL_RUN:
+        ML_pipeline(results, args, curr_run_dir, sample_frac=1.0, RFE=True, large_grid = True, include_output_tree_features= args.include_output_tree_features)
     logging.info(f"Working on MSA level features")
 
 
