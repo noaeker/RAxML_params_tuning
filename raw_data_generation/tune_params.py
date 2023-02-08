bccd955bed377dddfb26d4f664821a39edb77c29
noaeker <n12345e@gmail.com>
2023-02-08 18:31:23 +0200

    small fix

--- raw_data_generation/tune_params.py
+++ raw_data_generation/tune_params.py
@@ -98,6 +98,7 @@ def main():
         MSA_tasks = job_tasks_dict_per_MSA[MSA]
         msa_job_done_dict = {}
         start_time = time.time()
+        total_test_time = raxml_run_on_test_msa(args, tmp_starting_tree_path)
         for i, task_key in (enumerate(MSA_tasks)):
             if os.path.exists(job_local_stop_running_path):  # break out of the loop if all tasks are done
                 break
