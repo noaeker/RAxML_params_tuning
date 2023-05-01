00843316604e43d161fa1b89f74d77b785f64dc5
noaeker <n12345e@gmail.com>
2023-05-01 13:22:42 +0300

    small fix

--- groups_paper_ML_code/features_extactor_groups.py
+++ groups_paper_ML_code/features_extactor_groups.py
@@ -237,9 +237,9 @@ def generate_RF_distance_matrix_statistics_final_trees(curr_run_directory, final
     if len(final_trees)==1:
         return get_summary_statistics_dict(feature_name=f"{prefix}",values = None)
     RF_distance_mat = generate_RF_distance_matrix(curr_run_directory, final_trees)
+    all_results = {}
     try:
         mds_models = {f'{prefix}_mds_5':MDS(n_components=5, metric = False, dissimilarity='precomputed').fit_transform(RF_distance_mat),f'{prefix}_mds_3':MDS(n_components=3, metric = False, dissimilarity='precomputed').fit_transform(RF_distance_mat),f'{prefix}_mds_2':MDS(n_components=2, metric = False, dissimilarity='precomputed').fit_transform(RF_distance_mat),f'{prefix}_mds_1':MDS(n_components=1, metric = False, dissimilarity='precomputed').fit_transform(RF_distance_mat)}
-        all_results = {}
         for model in mds_models:
             fit_SVC(SVC(), mds_models[ model], best_tree, f"{model}_rbf_svc", all_results, True_global_data=None)
             clf = SVR()
